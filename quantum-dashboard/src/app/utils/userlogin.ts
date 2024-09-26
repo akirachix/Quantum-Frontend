83e6dab23fc2818ade338c4e977c7ef7a40c9687
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-09-26 22:44:00 +0300

    add login files

---
+++ quantum-dashboard/src/app/utils/userlogin.ts
@@ -0,0 +1,38 @@
+
+const url = '/api/login';
+export const userlogin = async (loginData: { email: string; password: string; }) => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+        });
+        return response.json();
+    } catch (error) {
+        throw new Error((error as Error).message);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
