c8cdaf2e25fdc994f61921440c64f9c6df787eac
Loice-Staice <staiceloicea@gmail.com>
2024-09-27 00:43:56 +0300

    Made changes

--- quantum-dashboard/src/app/page.tsx
+++ quantum-dashboard/src/app/page.tsx
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-import Image from "next/image";
-import RegisterFarmer from "./Registration";
-export default function Home() {
-  return (
- <main>
-   <RegisterFarmer/>
- </main>
-  );
-}
-=======
 import React from "react";
 import FarmersDetails from "./farmerdetails/page";
+import RegisterForm from "./Registration";
 
 function App() {
   return (
     <div className="App">
+      {/* <RegisterForm/> */}
       <FarmersDetails />
+
     </div>
   );
 }
 
 export default App;
->>>>>>> 94df123301192b4ce0601ecc21c457d1de7af914
