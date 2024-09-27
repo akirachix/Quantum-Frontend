4e6f2515c58b00ce72b3f507d20a5a959899f149
Esther-Naserian <yemanehaxumawit@gmail.com>
2024-09-28 00:46:40 +0300

    made changes

--- dashboard-quantum/src/app/login/page.tsx
+++ dashboard-quantum/src/app/login/page.tsx
@@ -101,12 +101,18 @@ const Login = () => {
             {isSubmitting ? "Logging you In...." : "Login"}
           </button>
           <button
-            type="button"
-            className="w-full py-2 mt-4 border border-gray-300 text-gray-700 font-semibold rounded-md hover:bg-gray-100 flex items-center justify-center ml-8" 
-          >
-            <img src="/images/google.png" alt="Google Logo" className="w-5 h-5 mr-2" />
-            Continue with Google
-          </button>
+  type="button"
+  className="w-full py-2 mt-4 border border-gray-300 text-gray-700 font-semibold rounded-md hover:bg-gray-100 flex items-center justify-center ml-8" 
+>
+  <Image 
+    src="/images/google.png" 
+    alt="Google Logo" 
+    width={20} 
+    height={20} 
+    className="mr-2" 
+  />
+  Continue with Google
+</button>
         </form>
       </div>
     </div>
