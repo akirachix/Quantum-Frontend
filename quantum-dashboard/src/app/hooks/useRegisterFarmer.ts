aab38d5a5da30c6104b20c88da56577a9aae2573
Loice-Staice <staiceloicea@gmail.com>
2024-09-27 21:12:55 +0300

    added changes

--- quantum-dashboard/src/app/hooks/useRegisterFarmer.ts
+++ quantum-dashboard/src/app/hooks/useRegisterFarmer.ts
@@ -1,37 +1,3 @@
-// import { useState } from 'react';
-// import { userRegister as registerAPI } from '@/app/utils/registerFarmer';
-// import { FarmerDetails } from '../utils/types';
-// const useRegister = () => {
-//   const [isSubmitting, setIsSubmitting] = useState(false);
-//   const [errorMessage, setErrorMessage] = useState('');
-//   const [successMessage, setSuccessMessage] = useState('');
-//   const userLogin = async (registerData: FarmerDetails) => {
-    
-//     try {
-//       setIsSubmitting(true);
-//       const { data, error } = await registerAPI(registerData);
-//       if (error) {
-//         setErrorMessage(error);
-//         setSuccessMessage('');
-//       } else {
-//         setSuccessMessage('User Registered successful!');
-//         setErrorMessage('');
-//         return data;
-//       }
-//     } catch (error) {
-//       setErrorMessage('An error occurred. Please try again.');
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
-//   return {
-//     isSubmitting,
-//     errorMessage,
-//     successMessage,
-//   };
-// };
-// export default useRegister;
-
 
 import { FarmerDetails } from '../utils/types';
 import { useState } from 'react';
